838bf48b4f72738f964302d8be02bb9321dc3c52
guilhermeHomma <guilhermeghomma@gmail.com>
2025-10-13 12:07:12 -0300

    add build

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+GAME_NAME="mobize"
+LOVE_WIN_DIR="./love-win"
+OUT_DIR="./dist-windows"
+
+# 1) .love
+rm -f "../$GAME_NAME.love"
+( cd ./ && zip -9 -r "../$GAME_NAME.love" . -x "*.git*" "*node_modules/*" "*__pycache__/*" "*.aseprite*")
+mv "../$GAME_NAME.love" "./$GAME_NAME.love"
+
+# 2) pasta de saída
+rm -rf "$OUT_DIR"
+mkdir -p "$OUT_DIR"
+
+# 3) gerar exe único
+cat "$LOVE_WIN_DIR/love.exe" "./$GAME_NAME.love" > "$OUT_DIR/$GAME_NAME.exe"
+
+# 4) copiar DLLs e licenças
+cp "$LOVE_WIN_DIR"/*.dll "$OUT_DIR"/
+cp "$LOVE_WIN_DIR"/LICENSE.txt "$OUT_DIR"/ || true
+cp "$LOVE_WIN_DIR"/changes.txt "$OUT_DIR"/ || true
+cp "$LOVE_WIN_DIR"/readme.txt "$OUT_DIR"/ || true
+
+echo "Build pronto em $OUT_DIR/"
